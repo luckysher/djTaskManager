8c4e541a7562341dd8f86556e363b8214d47e516
luckysher <luckyshera95@gmail.com>
2017-04-14 05:01:59 +0530

    updated dashboard admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -3,8 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from models import *
+from security.models import TaskUser
 
 # Register your models here.
 
-admin.site.register(User)
+admin.site.register(TaskUser)
 admin.site.register(Task)
